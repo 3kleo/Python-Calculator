fdfe6c9c889153da64c5aef258e22d81ccfa10e2
Leonardo Koakowski <82957998+3kleo@users.noreply.github.com>
2022-06-18 12:32:26 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -5,10 +5,38 @@ from kivy.core.window import Window
 
 
 class MyGrid(GridLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self._keyboard = Window.request_keyboard(self._keyboard_closed, self)
+        self._keyboard.bind(on_key_down=self._on_keyboard_down)
+
+    def _keyboard_closed(self):
+        self._keyboard.unbind(on_key_down=self._on_keyboard_down)
+        self._keyboard = None
+
+    def _on_keyboard_down(self, keyboard, keycode, text, modifiers):
+        if len(modifiers) == 0 and (keycode[1].isnumeric() or keycode[1] in ['-', '/', '.']):
+            self.display.text += keycode[1]
+        elif len(modifiers) > 0 and modifiers[0] == 'shift' and keycode[1] == '=':
+            self.display.text += '+'
+        elif len(modifiers) > 0 and modifiers[0] == 'shift' and keycode[1] == '8':
+            self.display.text += '*'
+        elif keycode[1] == 'enter':
+            self.calculate(self.display.text)
+        elif keycode[1] == 'escape':
+            self.display.text = ''
+        elif keycode[1] == 'backspace':
+            self.display.text = self.display.text[:-1]
+        return True
+
     def calculate(self, calculation):
         if calculation:
             try:
-                self.display.text = str(eval(calculation))
+                result = eval(calculation)
+                if float(result).is_integer():
+                    self.display.text = str(int(result))
+                else:
+                    self.display.text = str(round(result, 6))
             except Exception:
                 self.display.text = "Error"
 
